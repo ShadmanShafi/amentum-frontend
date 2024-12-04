3970085fe0a18a17a49bd00b003816e402eacaa3
shadmanshafi <shadmanshafi@iut-dhaka.edu>
2024-12-04 14:51:22 +0600

    styling: added scroolbar styling

--- src/pages/Vps/Details/index.tsx
+++ src/pages/Vps/Details/index.tsx
@@ -10,8 +10,14 @@ import {
   IconStatusWarningInactive,
 } from "@/assets/Icons";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useParams } from "react-router-dom";
 
 const VpsDetails = () => {
+  const { id } = useParams();
+
+  console.log("params: ", id);
+
   return (
     <div className="">
       <div className="flex items-center gap-8 px-14">
@@ -19,7 +25,7 @@ const VpsDetails = () => {
 
         <div className="flex flex-col gap-3">
           <h1 className="text-4xl font-bold tracking-tight scroll-m-20 text-customTextColor">
-            Mitarbeiter-Server
+            Mitarbeiter-Server - {id}
           </h1>
 
           <div className="flex gap-2">
@@ -30,11 +36,21 @@ const VpsDetails = () => {
         </div>
 
         <div className="flex flex-wrap gap-3 py-2">
-          <IconHardReset />
-          <IconNewInstallation />
-          <IconRescueSystem />
-          <IconPasswordReset />
-          <IconSoftReset />
+          <div className="transition-transform cursor-pointer hover:scale-105 active:scale-95">
+            <IconHardReset />
+          </div>
+          <div className="transition-transform cursor-pointer hover:scale-105 active:scale-95">
+            <IconNewInstallation />
+          </div>
+          <div className="transition-transform cursor-pointer hover:scale-105 active:scale-95">
+            <IconRescueSystem />
+          </div>
+          <div className="transition-transform cursor-pointer hover:scale-105 active:scale-95">
+            <IconPasswordReset />
+          </div>
+          <div className="transition-transform cursor-pointer hover:scale-105 active:scale-95">
+            <IconSoftReset />
+          </div>
         </div>
       </div>
 
@@ -124,7 +140,45 @@ const VpsDetails = () => {
         </div>
       </div>
 
-      <IconServerDetails />
+      <div className="flex justify-center px-4 py-6 sm:px-8 md:px-16 lg:px-32 xl:px-48">
+        <Tabs defaultValue="account" className="w-full">
+          <TabsList className="grid w-full grid-cols-5">
+            <TabsTrigger value="snapshots">Snapshots</TabsTrigger>
+            <TabsTrigger disabled value="images">
+              Images
+            </TabsTrigger>
+            <TabsTrigger disabled value="private-network">
+              Private network
+            </TabsTrigger>
+            <TabsTrigger disabled value="additional-ips">
+              Additional IPs
+            </TabsTrigger>
+            <TabsTrigger disabled value="licenses">
+              Licenses
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="snapshots">
+            <div>Table for Snapshots</div>
+          </TabsContent>
+
+          <TabsContent value="images">
+            <div>Content for Images</div>
+          </TabsContent>
+
+          <TabsContent value="private-network">
+            <div>Content for Private network</div>
+          </TabsContent>
+
+          <TabsContent value="additional-ips">
+            <div>Content for Additional IPs</div>
+          </TabsContent>
+
+          <TabsContent value="licenses">
+            <div>Content for Licenses</div>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 };
